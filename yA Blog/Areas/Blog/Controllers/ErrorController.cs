29a8a67865be5c8f729d45820363f50585884c42
Taha Yüksel <taha.yuksel053@gmail.com>
2018-01-17 18:00:42 +0300

    404 sayfası eklendi.Genel düzenlemeler yapıldı
    ...

---
+++ yA Blog/Areas/Blog/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yA_Blog.Areas.Blog.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
